40ec476aa788d816cd000a9a5f169a7ea4589fc6
TOxaREY <delta@toxarey.xyz>
2023-01-23 17:32:27 +0300

    sprint_08 v.1.0

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Anton Reynikov on 21.01.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+}
